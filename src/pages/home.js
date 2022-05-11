7fef9a618466951b3b2155f7edc803aa300f535b
Ahmedxd199 <ahmed_20170012@fci.helwan.edu.eg>
2022-05-11 03:12:46 +0200

    level 3 finich

---
+++ src/pages/home.js
@@ -0,0 +1,27 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Container } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+
+
+const Home = () => {
+
+ 
+return (
+    <>
+          <div style={{fontFamily: 'Oleo Script Swash Caps', textAlign:"center" , marginTop:"300px", fontSize:"1.5em"  }}>
+
+              <h1>Robusta Challenge</h1>
+
+          </div>
+
+    
+    </>
+); 
+
+  
+
+}
+
+export default Home;
+
+
